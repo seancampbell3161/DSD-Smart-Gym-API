6f54e0ff989c50de135f3e615ddaa40268582388
Alex Appleget <alexappleget2014@gmail.com>
2025-07-17 13:20:23 -0500

    installed clerk

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(clerkMiddleware());
 
 export default app;

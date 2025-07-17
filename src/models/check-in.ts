3617d5d6ad33b76db15b62fdc03568f002b33df0
Alex Appleget <alexappleget2014@gmail.com>
2025-07-17 10:11:41 -0500

    Renaming schema variables

--- src/models/check-in.ts
+++ src/models/check-in.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const checkInSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  gym: { type: Schema.Types.ObjectId, ref: "Gym", required: true },
+  user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  gym_id: { type: Schema.Types.ObjectId, ref: "Gym", required: true },
   checked_in: { type: Date, required: true },
   checked_out: { type: Date, default: null },
 });
--- src/models/qr-token.ts
+++ src/models/qr-token.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const qrTokenSchema = new Schema({
-  user: { tpye: Schema.Types.ObjectId, ref: "User", required: true },
-  gym: { type: Schema.Types.ObjectId, ref: "Gym", required: true },
+  user_id: { tpye: Schema.Types.ObjectId, ref: "User", required: true },
+  gym_id: { type: Schema.Types.ObjectId, ref: "Gym", required: true },
   token: { tpye: String, required: true, unique: true },
   expires_at: { type: Date, required: true },
   created_at: { type: Date, default: Date.now },

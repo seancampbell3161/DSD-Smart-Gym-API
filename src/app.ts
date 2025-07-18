d338c7f1acae9bbd8977b18dce72178f06c2d05b
Alex Appleget <alexappleget2014@gmail.com>
2025-07-18 14:50:55 -0500

    added cors to backend to be able to take requests from the frontend

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./connectDB";
 import { clerkMiddleware } from "@clerk/express";
@@ -8,6 +9,13 @@ import gymRoutes from "./routes/gyms";
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(clerkMiddleware());

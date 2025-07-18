9b27f8732b8873071b0a998d59b9d58a5a9a3bb1
Alex Appleget <alexappleget2014@gmail.com>
2025-07-18 12:05:39 -0500

    created fetch profile endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./connectDB";
 import { clerkMiddleware } from "@clerk/express";
-import userRoutes from "./routes/users";
+import profileRoutes from "./routes/profiles";
 import gymRoutes from "./routes/gyms";
+
 dotenv.config();
 
 const app = express();
@@ -13,7 +14,7 @@ app.use(clerkMiddleware());
 
 connectDB();
 
-app.use("/api/users", userRoutes);
+app.use("/api/profiles", profileRoutes);
 app.use("/api/gyms", gymRoutes);
 
 export default app;
